6c179718faecc2a6cc31f03cb664c7eecf94e9f0
MitchKubina <mtkubina6@gmail.com>
2025-06-15 17:30:04 -0600

    added tailwind

--- server/index.js
+++ server/index.js
@@ -1,11 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const cors = require("cors");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.get("/api/workouts", (req, res) => {
+  // Fetch workouts from DB
+});
