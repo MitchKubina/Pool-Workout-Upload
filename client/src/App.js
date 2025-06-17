1ac226f4a6e4f247770bb1d2224837be956bbabf
MitchKubina <mtkubina6@gmail.com>
2025-06-17 15:56:04 -0600

    tailwind is working now hooray!

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,9 @@
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-blue-100 flex items-center justify-center">
+      <div className="bg-white p-6 rounded shadow text-blue-600 text-xl font-bold">
+        Tailwind is working in React!
+      </div>
     </div>
   );
 }
